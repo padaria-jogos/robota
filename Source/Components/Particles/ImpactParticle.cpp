cc4c7618fd7b78ae730104bd24a091bbc2e002a2
andreassisrg <andreassisrg@gmail.com>
2025-11-29 16:54:47 -0300

    refatoracao sistema de particulas

---
+++ Source/Components/Particles/ImpactParticle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andre on 29/11/2025.
+//
+
+#include "ImpactParticle.h"
---
+++ Source/Components/Particles/ImpactParticle.h
@@ -0,0 +1,13 @@
+//
+// Created by andre on 29/11/2025.
+//
+
+#ifndef ROBOTA_IMPACTPARTICLE_H
+#define ROBOTA_IMPACTPARTICLE_H
+
+
+class ImpactParticle {
+};
+
+
+#endif //ROBOTA_IMPACTPARTICLE_H
